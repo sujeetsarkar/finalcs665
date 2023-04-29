cbce4c7985686a6683d135657ed86e7872e3526a
AI admin <admin@auxiliuminfotech.com>
2023-04-29 11:56:25 +0530

    changes

--- src/sujeet/cs665/Chairperson.java
+++ src/sujeet/cs665/Chairperson.java
@@ -1,72 +1,16 @@
 package sujeet.cs665;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Chairperson extends Faculty {
-    private List<Course> coursesOffered;
-    private Map<String, ConcentrationGroup> concentrations;
-    private List<Faculty> fullTimeFaculty;
-    private List<Faculty> partTimeFaculty;
-    private Faculty graduateAdvisor;
-    private Faculty undergraduateAdvisor;
+	private String name;
+	private Course courseTaught;
 
     public Chairperson(String name, Department department) {
     	super(name, department, Boolean.TRUE); // Chairperson will always be fulltime
-        coursesOffered = new ArrayList<>();
-        concentrations = new HashMap<>();
-        fullTimeFaculty = new ArrayList<>();
-        partTimeFaculty = new ArrayList<>();
+    	this.name = name;
     }
 
     public void addCourseTaught(Course course) {
-        coursesOffered.add(course);
-        System.out.println("Faculty");
-    }
-
-    public void addConcentration(ConcentrationGroup concentration) {
-        concentrations.put(concentration.getTitle(), concentration);
-    }
-
-    public void addFullTimeFaculty(Faculty faculty) {
-        fullTimeFaculty.add(faculty);
-    }
-
-    public void addPartTimeFaculty(Faculty faculty) {
-        partTimeFaculty.add(faculty);
-    }
-
-    public void setGraduateAdvisor(Faculty faculty) {
-        graduateAdvisor = faculty;
-    }
-
-    public void setUndergraduateAdvisor(Faculty faculty) {
-        undergraduateAdvisor = faculty;
-    }
-
-    public List<Course> getCoursesOffered() {
-        return coursesOffered;
-    }
-
-    public ConcentrationGroup getConcentration(String name) {
-        return concentrations.get(name);
-    }
-
-    public List<Faculty> getFullTimeFaculty() {
-        return fullTimeFaculty;
-    }
-
-    public List<Faculty> getPartTimeFaculty() {
-        return partTimeFaculty;
-    }
-
-    public Faculty getGraduateAdvisor() {
-        return graduateAdvisor;
-    }
-
-    public Faculty getUndergraduateAdvisor() {
-        return undergraduateAdvisor;
+    	courseTaught = course;
+        System.out.println("Course " + course.getTitle() + " assigned to chairman " + name);
     }
 }
--- src/sujeet/cs665/MainApp.java
+++ src/sujeet/cs665/MainApp.java
@@ -3,51 +3,6 @@ package sujeet.cs665;
 public class MainApp {
 
 	public static void main(String[] args) {
-//		CourseFactory coreCourseFactory = new CoreCourseFactory();
-//        CourseFactory electiveCourseFactory = new ElectiveCourseFactory();
-//
-//        Course coreCourse1 = coreCourseFactory.createCourse("Data Structures", "Description of Data Structures", "Syllabus for Data Structures");
-//        Course electiveCourse1 = electiveCourseFactory.createCourse("Artificial Intelligence", "Description of AI", "Syllabus for AI");
-//        
-//        // use the created courses as required
-//        
-//        Department department = ComputerScienceDepartment.getInstance();
-//        department.addCourse(coreCourse1);
-//        department.addCourse(electiveCourse1);
-//        Student alice = new Student("Alice Smith", "alice@example.com", department);
-//        Student bob = new Student("Bob Johnson", "bob@example.com", department);
-//        department.addStudent(alice);
-//        department.addStudent(bob);
-//        Faculty profX = new Faculty("Prof. X", department);
-//        Faculty profY = new Faculty("Prof. Y", department);
-//        department.addFaculty(profX);
-//        department.addFaculty(profY);
-//        Chairperson chairperson = new Chairperson("Prof. Z", department);
-//        department.addChairperson(chairperson);
-//        GraduateAdvisor gradAdvisor = new GraduateAdvisor("Prof. A", department);
-//        department.addFaculty(gradAdvisor);
-//        UndergraduateAdvisor undergradAdvisor = new UndergraduateAdvisor("Prof. B", department);
-//        department.addFaculty(undergradAdvisor);
-//        // Other code for managing courses, students, and faculty
-//        
-////        Course course = new CourseProxy("Java Programming", "Syllabus for Java Programming");
-////        System.out.println(course.getFormattedContent());
-		//---------------------------------------------------------
-//		Chairperson chairperson = new Chairperson("John Doe", ComputerScienceDepartment.getInstance());
-//		Course introToCS = new CourseProxy("CS101","Introduction to Computer Science","Test Syallbus", 20);
-//		Course databaseSystems = new CourseProxy("CS202", "Database Systems", "Database Syallbus", 20);
-//		chairperson.addCourse(introToCS);
-//		chairperson.addCourse(databaseSystems);
-//		Student student1 = new Student("Alice", "CS", 123);
-//		Student student2 = new Student("Bob", "CS", 456);
-//		introToCS.enroll(student1);
-//		introToCS.enroll(student2); // this should work fine, since the limit is not reached
-//		introToCS.enroll(new Student("Charlie", "CS", 789)); // this should work fine too
-//		introToCS.enroll(new Student("David", "CS", 111)); // this should fail, since the limit is reached
-//		introToCS.drop(student1); // student1 drops the course
-//		Student student3 = new Student("Eve", "CS", 222);
-//		introToCS.enroll(student3); // student3 is added from the waitlist
-
 		// Create a computer science department
         Department csDepartment = ComputerScienceDepartment.getInstance();
 
@@ -111,11 +66,6 @@ public class MainApp {
         // The chairperson should not be notified because the enrollment limit has not been reached
 
         // Enroll more students in the course until the enrollment limit is reached
-        for (int i = 0; i < 9; i++) {
-            Student student = new Student("Student " + i, "s" + i, csDepartment);
-            csDepartment.addStudent(student);
-            student.enrollCourse(course1);
-        }
         // The chairperson should be notified because the enrollment limit has been reached
 
         // Waitlist a student for the course
